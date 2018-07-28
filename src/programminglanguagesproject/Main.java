5e096ed4b80e6b6b051c2ab498c29aa07a0ddcbe
tojasser <tojasser@gmail.com>
2018-07-28 17:45:04 +0300

    connect GUI ,there's some cases need to be handled

--- src/programminglanguagesproject/Main.java
+++ src/programminglanguagesproject/Main.java
@@ -6,6 +6,7 @@
 package programminglanguagesproject;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.regex.*;
 
 /**
@@ -15,32 +16,83 @@ import java.util.regex.*;
 
 public class Main {
     
-    static MainFrame mainFrame;
+    static MainJFrame mainFrame;
+    static LinkedList lexeme = new LinkedList();
+    static LinkedList token = new LinkedList();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         
-        mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
+    
+     
+       
         
+        mainFrame= new MainJFrame();
+         mainFrame.setVisible(true);
+       
+   
+       
+      
     }
 
+    
         
  
     static boolean FilterInput(String input)
     {
         //This is a regex to check if the input is written in the correct way
-        
         // the general form is <Variable> = <anything> ; also any white space is allowed 
-        Pattern p = Pattern.compile("[a-z]\\s?=\\s?(.*?)\\s?;\\n?");
+        Pattern p = Pattern.compile("[a-z]*\\s?=\\s?(.*?)\\s?;\\n?");
         Matcher m = p.matcher(input);
-        
         return m.matches();
     }
     
-
+    static void compile(String statement){
+        
+        String [] list = statement.split(" ");
+        Pattern p = Pattern.compile("\\d+");
+        
+        for (int i = 0; i < list.length; i++) {
+            String s = list[i];
+            Matcher m = p.matcher(s);
+            lexeme.add(i,s);
+            
+            
+        
+            if (m.find())//if find number
+                token.add(i,"INT_LIT");
+            else{
+            switch (s){
+                case "=":
+                    token.add(i,"ASSIGN_OP");
+                    break;
+                case "(":
+                    token.add(i,"LEFT_PAREN");    
+                    break;
+                case "-":
+                    token.add(i,"SUB_OP");
+                    break;
+                case ")":
+                    token.add(i,"RIGHT_PAREN");
+                    break;
+                case "*":
+                    token.add(i,"MULT_OP");
+                    break;
+                case "[0-9]+":
+                    token.add(i,"INT_LIT");
+                    break;
+                case ";":
+                    token.add(i,"SEMI_COL");
+                    break;
+                default:
+                    token.add(i, "IDENT");
+                    
+                }               
+            }
+        }
+    }
     
     
        
@@ -54,8 +106,8 @@ public class Main {
            if(! FilterInput(statement.trim()))
             return lineNumber + " | " + statement.trim() +" <= Incorrect expersion";
            
-           //Statement is correct so Start compiling :)
-          
+           //Statement is correct so Start compiling :)           
+           compile(statement);
            
            
            
@@ -65,6 +117,10 @@ public class Main {
         }
             return "Correct expersion";
         }
+        
+        
+ 
+      
 }
         
 
